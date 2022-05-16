c2bcd37e8bfea77354eec0c89d106b6e00a17a96
lorenzas <lorenza@ciandt.com>
2022-05-16 17:12:25 -0300

    Atualizando task 5

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,8 +1,12 @@
+import java.util.Locale;
+import java.text.NumberFormat;
 public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        System.out.printf("Valor: " + String.format("R$ %,.2f",valor));
+        NumberFormat formatter = NumberFormat.getInstance(new Locale("en","US"));
+        formatter.setMaximumFractionDigits(2);
+        System.out.printf("Valor: R$ " + formatter.format(valor));
         System.out.printf("\nTaxa: %.2f%%\n", taxa);
     }
 }
