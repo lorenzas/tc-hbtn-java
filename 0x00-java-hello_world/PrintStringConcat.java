ddc8cc3b0b31279ed4c51f02137dd12ea1272cda
lorenzas <lorenza@ciandt.com>
2022-05-16 11:41:15 -0300

    Adicionando task 4

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -3,6 +3,6 @@ public class PrintStringConcat {
         String texto1 = "Holberton";
         String texto2 = "School";
         String textoCompleto = texto1 + " " + texto2;
-        System.out.println("Bem vindo a " + textoCompleto);
+        System.out.println("Bem vindo a " + textoCompleto + "!");
     }
 }
