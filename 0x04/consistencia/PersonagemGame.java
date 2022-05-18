806cd0ba789a9d0852165cbf7b41aa8eea567511
lorenzas <lorenza@ciandt.com>
2022-05-18 12:30:34 -0300

    Adicionando task 4

---
+++ 0x04/consistencia/PersonagemGame.java
@@ -0,0 +1,45 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        setSaudeAtual(saudeAtual);
+        setNome(nome);
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (saudeAtual > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null && !nome.isBlank()) {
+            this.nome = nome;
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        setSaudeAtual(Math.max(saudeAtual - quantidadeDeDano, 0));
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        setSaudeAtual(Math.min(saudeAtual + quantidadeDeCura, 100));
+    }
+}
