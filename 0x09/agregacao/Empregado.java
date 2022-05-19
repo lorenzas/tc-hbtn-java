1495021f43e28440f6ec13ab9cf5197a735c3cde
lorenzas <lorenza@ciandt.com>
2022-05-19 11:08:42 -0300

    Adicionando task 0

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,43 @@
+public class Empregado {
+    private String nome;
+    private int codigo;
+    private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Empregado(String nome, int codigo, Endereco endereco) {
+        setNome(nome);
+        setCodigo(codigo);
+        setEndereco(endereco);
+    }
+
+    public void apresentar() {
+        System.out.println("Codigo: " + this.getCodigo() + "\n" +
+                           "Nome: " + this.getNome() + "\n" +
+                           "Bairro: " + this.getEndereco().getBairro() + "\n" +
+                           "Cidade: " + this.getEndereco().getCidade() + "\n" +
+                           "Pais: " + this.getEndereco().getPais());
+    }
+}
