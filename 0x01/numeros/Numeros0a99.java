6dfe889bb7edb2b1c94b52907c79369501b6c7d1
lorenzas <lorenza@ciandt.com>
2022-05-23 10:11:38 -0300

    Alterando task 0

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,8 +1,6 @@
-package numeros;
-
 public class Numeros0a99 {
     public static void main(String[] args) {
-        for (int i = 1; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             if (i < 99) {
                 System.out.print(i + ", ");
             } else {
