7b3b96028ab9152c37ac29c10e0e6f2f0eda5a14
lorenzas <lorenza@ciandt.com>
2022-05-23 10:10:35 -0300

    Adicionando task 0

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,13 @@
+package numeros;
+
+public class Numeros0a99 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            if (i < 99) {
+                System.out.print(i + ", ");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
