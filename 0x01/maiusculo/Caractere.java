a4548647a25279bd7c517664ac7ceee0ee74033d
lorenzas <lorenza@ciandt.com>
2022-05-23 10:26:59 -0300

    Alterando task 2

--- 0x01/maiusculo/Caractere.java
+++ 0x01/maiusculo/Caractere.java
@@ -1,6 +1,6 @@
 public class Caractere {
     public static boolean EhMaiusculo(char c) {
-        if (ch >= 'A' && ch <= 'Z') {
+        if (c >= 'A' && c <= 'Z') {
             return true;
         } else {
             return false;
