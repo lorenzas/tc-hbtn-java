2b85e76b00a7a7a970dc4a49158f8831a946afb2
lorenzas <lorenza@ciandt.com>
2022-05-24 10:46:56 -0300

    Adicionando task 0

---
+++ 0x06/pedido_livraria/Pedido.java
@@ -0,0 +1,33 @@
+public class Pedido {
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public void setPercentualDesconto(double percentualDesconto) {
+        this.percentualDesconto = percentualDesconto;
+    }
+
+    public ItemPedido[] getItens() {
+        return itens;
+    }
+
+    public void setItens(ItemPedido[] itens) {
+        this.itens = itens;
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (ItemPedido item : getItens()) {
+            total += (item.getProduto().obterPrecoLiquido() * item.getQuantidade());
+        }
+        return (total > 0) ? total - total * (percentualDesconto/100) : 0;
+    }
+}
