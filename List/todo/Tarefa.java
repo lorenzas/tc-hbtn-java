b3bdabca6611faee553b9a12152907e49193d8b3
lorenzas <lorenza@ciandt.com>
2022-05-30 16:41:50 -0300

    Adicionando task 2

---
+++ List/todo/Tarefa.java
@@ -0,0 +1,42 @@
+public class Tarefa {
+    private String descricao;
+    private boolean estahFeita;
+    private int identificador;
+
+    public Tarefa(String descricao, int identificador) {
+        setDescricao(descricao);
+        this.identificador = identificador;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        if (!descricao.isEmpty()) {
+            this.descricao = descricao;
+        } else {
+            throw new IllegalArgumentException("Descricao de tarefa invalida");
+        }
+    }
+
+    public boolean isEstahFeita() {
+        return estahFeita;
+    }
+
+    public void setEstahFeita(boolean estahFeita) {
+        this.estahFeita = estahFeita;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public void modificarDescricao(String descricao) {
+        setDescricao(descricao);
+    }
+}
