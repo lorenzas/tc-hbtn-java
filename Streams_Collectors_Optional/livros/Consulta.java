c2fecb23b0ec85392adc61c8f52c266c7a73dd21
lorenzas <lorenza@ciandt.com>
2022-06-13 15:17:56 -0300

    Adicionando task 0

---
+++ Streams_Collectors_Optional/livros/Consulta.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream().filter(produto -> produto.getCategoria().equals(CategoriaProduto.LIVRO)).collect(Collectors.toList());
+    }
+}
